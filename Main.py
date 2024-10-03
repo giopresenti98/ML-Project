6be92904e5f951e0bbf37aca2bc5684d91a819dd
Simone Piras <simone95piras@gmail.com>
2024-10-03 23:17:01 +0200

    format edit

--- Main.py
+++ Main.py
@@ -5,13 +5,17 @@ import numpy as np
 import Plot
 from Classifiers import knn
 import os
+
+nRows = 25000
+
+# Load the dataset
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'Dataset/data.csv')
-nRows = 25000
-# Load the dataset
-data = pd.read_csv(filename,
-                   on_bad_lines='skip', nrows=nRows*10)
-data_shuffled=data.sample(frac=1)
+data = pd.read_csv(filename, on_bad_lines='skip', nrows=nRows*10)
+
+# Shuffle the dataset
+data_shuffled = data.sample(frac=1)
+
 # n_samples=int(len(data['password']))
 
 # Generate features
@@ -25,6 +29,8 @@ ytr = data_shuffled['strength'].head(nRows).values
 print(xtr)
 print(xtr.shape)
 
+# Train the model
 clf = knn.train(xtr, ytr)
 
+# Plot the decision regions
 Plot.plot_decision_regions(clf, xtr, ytr, resolution=0.1)
