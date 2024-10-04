ad1ed9a2315254fdeb26f9c59aa802d62ad66ed4
Simone Piras <simone95piras@gmail.com>
2024-10-04 12:20:30 +0200

    code reogranization

---
+++ Data.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def feature_extractor(data,nRows):
+    # Shuffle the dataset
+    data_shuffled = data.sample(frac=1)
+
+    # n_samples=int(len(data['password']))
+
+    # Generate features
+    pass_length = (data_shuffled['password'].str.len().head(nRows).values).T
+    numeric = (data_shuffled['password'].apply(lambda x: len([str(x)
+            for x in list(x) if str(x).isdigit()])).head(nRows).values).T
+    # Split the dataset into features and target variable
+    xtr = np.array([pass_length, numeric]).T
+    ytr = data_shuffled['strength'].head(nRows).values
+    return (xtr, ytr)
--- Main.py
+++ Main.py
@@ -1,30 +1,19 @@
 import pandas as pd
-import numpy as np
 
 
+import Data
 import Plot
 from Classifiers import knn
 import os
 
-nRows = 25000
+nRows = 2500
 
 # Load the dataset
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'Dataset/data.csv')
 data = pd.read_csv(filename, on_bad_lines='skip', nrows=nRows*10)
 
-# Shuffle the dataset
-data_shuffled = data.sample(frac=1)
-
-# n_samples=int(len(data['password']))
-
-# Generate features
-pass_length = (data_shuffled['password'].str.len().head(nRows).values).T
-numeric = (data_shuffled['password'].apply(lambda x: len([str(x)
-           for x in list(x) if str(x).isdigit()])).head(nRows).values).T
-# Split the dataset into features and target variable
-xtr = np.array([pass_length, numeric]).T
-ytr = data_shuffled['strength'].head(nRows).values
+(xtr,ytr)=Data.feature_extractor(data,nRows)
 
 print(xtr)
 print(xtr.shape)
