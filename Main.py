9f0953e99dfdfb5f37f0a6f5d7fbed5b84b4b752
giovanni presenti <g.presenti@studenti.unica.it>
2024-10-14 17:24:14 +0200

    Added 4th feature.

--- Main.py
+++ Main.py
@@ -5,32 +5,25 @@ from Classifiers import knn, svm, kde
 import os
 from sklearn.metrics import accuracy_score
 
-n_train = 500000
 # Load the dataset
+n_train = 500000
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'Dataset/data.csv')
 data = pd.read_csv(filename, on_bad_lines='skip')
-#print(data)
 (xtr, ytr, xts, yts)=Data.feature_extractor(data,n_train)
 
-
-
-#print(xtr)
-#print(xtr.shape)
-
 # Train the model
+
 clf = knn.knn_train(xtr, ytr)
+print("Accuracy of KNN: ", accuracy_score(yts, clf.predict(xts)))
 #clf2 = svm.svm_train(xtr, ytr)
+#print("Accuracy of SVM: ", accuracy_score(yts, clf2.predict(xts)))
 #clf3 = kde.kde_train(xtr, ytr)
-#print("Accuracy of KNN: ", accuracy_score(yts, clf.predict(xts)))
+#print("Accuracy of KDE: ", accuracy_score(yts, clf3.predict(xts)))
 
 # Plot the decision regions
 
-Plot.plot_decision_regions(clf, xts, yts, resolution=0.1, title='KNN, Accuracy: ' + str(accuracy_score(yts, clf.predict(xts))))
-#Plot.plot_decision_regions(clf2, xtr, ytr, resolution=0.1)
-#Plot.plot_decision_regions(clf3, xtr, ytr, resolution=0.1)
-
-#print("Accuracy of KNN: ", accuracy_score(yts, clf.predict(xts)))
-#print("Accuracy of SVM: ", accuracy_score(yts, clf2.predict(xts)))
-#print("Accuracy of KDE: ", accuracy_score(yts, clf3.predict(xts)))
+#Plot.plot_decision_regions(clf, xts, yts, resolution=0.1, title='KNN, Accuracy: ' + str(accuracy_score(yts, clf.predict(xts))))
+#Plot.plot_decision_regions(clf2, xtr, ytr, resolution=0.1, title='SVM, Accuracy: ' + str(accuracy_score(yts, clf.predict(xts))))
+#Plot.plot_decision_regions(clf3, xtr, ytr, resolution=0.1, title='KDE, Accuracy: ' + str(accuracy_score(yts, clf.predict(xts))))
  
